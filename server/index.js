85cb62fd38121697ebad471e6bc8fc652ca56e10
Josue Arredondo <josue.e.arredondo@gmail.com>
2022-03-09 11:16:44 -0700

    directory path for delpoyement

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@ app.use(express.json())
 app.use(cors());
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/Working-files/client/index.html"));
+  res.sendFile(path.join(__dirname, "/client/index.html"));
 });
 
-app.use(express.static(path.join(__dirname, "/Working-files/client")));
+app.use(express.static(path.join(__dirname, "/client")));
 // app.use(express.static(path.join(__dirname, "../")));
 
 const port = process.env.PORT || 5400;
